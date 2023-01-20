d9bd70d490532098534e8c9778811a71ba103ad6
Omar Chaar <oishaar@gmail.com>
2023-01-20 13:20:47 -0300

    live domain

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCq44TtfAq6DE1Alp4qojB6z5WwWHCvpbc",
+  authDomain: "contractgenerator-14c7d.firebaseapp.com",
+  projectId: "contractgenerator-14c7d",
+  storageBucket: "contractgenerator-14c7d.appspot.com",
+  messagingSenderId: "351431592795",
+  appId: "1:351431592795:web:c7598a2d2917169b8a687e",
+  measurementId: "G-C1N8CW9088"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @NgModule({
   declarations: [
